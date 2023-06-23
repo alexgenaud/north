276a286e18815ce900bee7ffe86be64f998fdefb
Alexander E Genaud <alex@genaud.net>
2023-06-23 10:14:03 +0200

    Docstring Interpreter and Shell

--- north/interpreter.py
+++ north/interpreter.py
@@ -7,11 +7,27 @@ from north.util import isInt
 
 class Interpreter:
     def __init__(self, dictionary=None, stack=None, memory=None):
+        """
+        Initializes the Interpreter.
+
+        Args:
+            dictionary (Dictionary, optional): The dictionary to use. Defaults to None.
+            stack (Stack, optional): The stack to use. Defaults to None.
+            memory (Memory, optional): The memory to use. Defaults to None.
+        """
         self.memory = Memory() if memory is None else memory
         self.dictionary = Dictionary(self.memory) if dictionary is None else dictionary
         self.stack = Stack() if stack is None else stack
 
     def execute_colon_word(self, address_list):
+        """
+        Executes a colon word definition.
+
+        Args:
+            address_list (list): The list of addresses representing the colon word definition.
+                                 The addresses point to int, callable, or colon-words
+                                 (which are further lists of addresses)
+        """
         MEM_0_IF = 0
         MEM_1_ELSE = 1
         MEM_2_THEN = 2
