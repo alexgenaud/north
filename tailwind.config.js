a7095bd78fb92af143e5f33061af87cc087052c0
Alexander E Genaud <alex@genaud.net>
2023-07-29 20:38:52 +0200

    Tailwind grid safelist col-span-16

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,19 @@ export default {
   content: ["./src/**/*.{html,js,jsx,tsx,ts}"],
   safelist: [
     {
-      pattern: /col-span-./,
-      pattern: /col-span-1./,
+      pattern: /^col-span-([1-9]|1[0-6])$/,
     },
   ],
   theme: {
     extend: {
       gridTemplateColumns: {
-        // Simple 16 column grid
         16: "repeat(16, minmax(0, 1fr))",
+      },
+      gridColumn: {
+        "span-13": "span 13 / span 13",
+        "span-14": "span 14 / span 14",
+        "span-15": "span 15 / span 15",
+        "span-16": "span 16 / span 16",
       },
     },
   },
