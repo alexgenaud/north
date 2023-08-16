a2ff2c7f462527db1a27f8c74235f2ed18128a85
Alexander E Genaud <alex@genaud.net>
2023-08-16 21:30:42 +0200

    Responsive font-size, toggle hex adr, val

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,6 @@ import FormInput from "./FormInput";
 import MemoryGrid from "./MemoryGrid";
 
 function App() {
-  console.log("App constructor");
   return (
     <div className="content-center border-sky-500">
       <MemoryGrid />
